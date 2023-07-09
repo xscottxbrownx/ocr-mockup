347b053c27c56be74072ba78297d0d88f1d29ed0
veganedge <scottxbrown@yahoo.com>
2023-07-09 13:50:05 -0700

    call to action 2 section

--- src/components/CallToAction2.jsx
+++ src/components/CallToAction2.jsx
@@ -1,4 +1,11 @@
+// Material UI Imports
 import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+// Material UI Imports
+import Stack from "@mui/material/Stack";
+// Material Image Imports
+import Image from "mui-image";
 
 export default function CallToAction2() {
   return (
@@ -6,13 +13,58 @@ export default function CallToAction2() {
       sx={{
         minHeight: "30rem",
         width: "100vw",
-        backgroundColor: "tertiary.main",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        padding: "3rem 10rem",
+        textAlign: "center",
       }}
     >
-      <h2>CTA2 SECTION</h2>
+      <Box>
+        <Stack
+          direction="row"
+          divider={<Divider orientation="vertical" flexItem />}
+          spacing={4}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Image src="my-image.png" width={300} showLoading shift="left" />
+            <Typography variant="h4" component="h3" color="primary" mt={2}>
+              CHOOSE YOUR CHALLENGE
+            </Typography>
+            <Typography variant="h5" component="p" mt={2} textAlign="center">
+              Getting Rugged? Challenge accepted! The course is designed so you
+              can choose your own destiny and difficulty – whether that’s
+              traversing over every obstacle at blistering speeds or skipping
+              one when you need to – victory awaits at the finish line no matter
+              what.
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              maxWidth: "50%",
+            }}
+          >
+            <Image src="my-image.png" width={300} showLoading shift="left" />
+            <Typography variant="h4" component="h3" color="primary" mt={2}>
+              MUDDY BACK GUARANTEE
+            </Typography>
+            <Typography variant="h5" component="p" mt={2} textAlign="center">
+              This year, we are offering a Muddy Back Guarantee – where we
+              guarantee everyone who participates will get muddier than any
+              Rugged Maniac event they’ve ever attended! If you don’t get muddy,
+              we’ll get you covered. Let our staff know and you’ll be
+              automatically upgraded to Rugged Maniac X on race day (meaning you
+              can run the event again, and track through even more mud!).
+            </Typography>
+          </Box>
+        </Stack>
+      </Box>
     </Box>
   );
 }
