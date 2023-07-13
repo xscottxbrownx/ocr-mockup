f91df9f5c6c12e14df3aeaaf6701005d67a10190
veganedge <scottxbrown@yahoo.com>
2023-07-12 18:03:14 -0700

    created obstacle list array constant

---
+++ src/constants/ObstacleList.js
@@ -0,0 +1,76 @@
+const OBSTACLES = [
+  {
+    id: 1,
+    name: "Wall",
+    image: "my-image.png",
+    blurb: "It's a wall",
+  },
+  {
+    id: 2,
+    name: "Mud Pit",
+    image: "my-image.png",
+    blurb: "It's a mud pit",
+  },
+  {
+    id: 3,
+    name: "Barbed Wire",
+    image: "my-image.png",
+    blurb: "It's barbed wire",
+  },
+  {
+    id: 4,
+    name: "Wall",
+    image: "my-image.png",
+    blurb: "It's a wall",
+  },
+  {
+    id: 5,
+    name: "Mud Pit",
+    image: "my-image.png",
+    blurb: "It's a mud pit",
+  },
+  {
+    id: 6,
+    name: "Barbed Wire",
+    image: "my-image.png",
+    blurb: "It's barbed wire",
+  },
+  {
+    id: 7,
+    name: "Wall",
+    image: "my-image.png",
+    blurb: "It's a wall",
+  },
+  {
+    id: 8,
+    name: "Mud Pit",
+    image: "my-image.png",
+    blurb: "It's a mud pit",
+  },
+  {
+    id: 9,
+    name: "Barbed Wire",
+    image: "my-image.png",
+    blurb: "It's barbed wire",
+  },
+  {
+    id: 10,
+    name: "Wall",
+    image: "my-image.png",
+    blurb: "It's a wall",
+  },
+  {
+    id: 11,
+    name: "Mud Pit",
+    image: "my-image.png",
+    blurb: "It's a mud pit",
+  },
+  {
+    id: 12,
+    name: "Barbed Wire",
+    image: "my-image.png",
+    blurb: "It's barbed wire",
+  },
+];
+
+export default OBSTACLES;
